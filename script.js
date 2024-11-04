3e0024f6c0677d6e6b7e2dfd9488c13fdf2dbf28
Path <57870461+DeniedPath@users.noreply.github.com>
2024-11-04 13:11:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Array to store talent data
+const talentData = [
+    {
+      experienceLevel: "Entry-level", // Level of experience
+      jobOpenings: 5000,               // Available job openings
+      availableTalent: 8000             // Candidates available
+    },
+    {
+      experienceLevel: "Mid-level",
+      jobOpenings: 3500,
+      availableTalent: 4000
+    },
+    {
+      experienceLevel: "Senior-level",
+      jobOpenings: 1500,
+      availableTalent: 1000
+    }
+  ];
